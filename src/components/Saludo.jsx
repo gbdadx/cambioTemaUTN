01e957c34e2f241b86e1411e0e63fb749dd8c0ab
gabriela diaz <gbdadx@gmail.com>
2023-11-16 13:05:51 -0300

    first commit

---
+++ src/components/Saludo.jsx
@@ -0,0 +1,19 @@
+export default function Saludo() {
+  return (
+    <>
+      <h1>Cambiar color y fuente</h1>
+      <p>cuando haga click, cambiarn la fuente y el color de la pagina</p>
+
+      <br />
+      <button onClick={cambiar}>Click!</button>
+    </>
+  );
+}
+
+const cambiar = () => {
+  const pagina = document.querySelector("#root");
+  // pagina.classList.contains('dark')?(pagina.classList.add('light'), pagina.classList.remove('dark')):(pagina.classList.add('dark'), pagina.classList.remove('light'))
+  // mas sencillo con toggle
+  pagina.classList.toggle("dark");
+  pagina.classList.toggle("light");
+};
